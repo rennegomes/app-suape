5e9717cda8448b366a33808c3c7491701bd7bf97
rennegomes <101829531+rennegomes@users.noreply.github.com>
2023-10-06 01:48:09 -0300

    Presentation

--- js/index.js
+++ js/index.js
@@ -1,48 +1,109 @@
+function openUrl(id){	
+	setCookie('id',id);
+	open('perfilCom.html','_self');
+}
+function abreUrl(id){	
+	setCookie('id',id);
+	open('res.html','_self');
+}
+
+let id;
+function loadResInfo(){
+	 let id = getCookie("id");
+	 buscarRestPorId(id);
+}
+
+function loadCategoInfo(){
+  let id = getCookie("id");
+  buscarCategoPorId(id);
+}
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 let divLogoRestaurante = document.querySelector("#logo"); // res.html.
-
-
-
     fetch("categorias.json").then((response) => {
         response.json().then((dados) => {
             dados.restaurantes.map((restaurante) => {
                 divLogoRestaurante.innerHTML += `
                 <div class=" col-6" >
-                <a href="perfilCom.html"><button class="restaurante caixaLogo" 
-                style="background-image:url(icones/overlay.png), url(${restaurante.logo});">ㅤ</button>
-                </a>
-               </div>`
-                
+
+               <button class="restaurante caixaLogo" onclick="openUrl(${restaurante.id});"
+                style="background-image:url(icones/overlay.png), url(${restaurante.logo});" >ㅤ</button> 
+
+
+               </div>
+               
+               <div class="col-6">
+               <p style="color:#fff;">
+               
+               ${restaurante.nome}</p>
+               
+               </div>
+
+               `
             })
         })
     });
+    
+let divLogo =  document.querySelector("#logoPerfil");
+let divTitulo=  document.querySelector("#tituloPerfil");
+let listaLogoRestaurante;
 
-    let divBanners = document.querySelector("#banners");
-
-
-
+let divBanners = document.getElementById("banners");
     fetch("categorias.json").then((response) => {
         response.json().then((dados) => {
             dados.banners.map((banners) => {
                 divBanners.innerHTML += `
                 <div class=" col-6" >
-                <a href="res.html">
-                <button class="fotoMiniatura escolha" style="background-image: url(icones/overlay.png), url(${banners.banner});">
-                <strong>${banners.titulo}</strong></button></a>
+               
+                <button class="fotoMiniatura escolha" onclick="abreUrl(${banners.id});" style="background-image: url(${banners.banner});">
+                <strong>${banners.titulo}</strong></button>
+				
                 </div>`
-                
             })
         })
     });
+  //   let divVolta = Document.getElementById("volta");
+  //   fetch("categorias.json").then((response) => {
+  //     response.json().then((dados) => {
+  //         dados.banners.map((banners) => {
+  //             divBanners.innerHTML += `
+  //             <div class=" col-6" >
+             
+  //             <button class="fotoMiniatura escolha" onclick="abreUrl(${banners.id});" style="background-image: url(icones/overlay.png), url(${banners.banner});">
+  //             <strong>${banners.titulo}</strong></button>
+      
+  //             </div>`
+  //         })
+  //     })
+  // });
 
-    
-
-    let divRestaurantes = document.querySelector("#local")
-
-// Função para buscar uma pessoa pelo nome
-function buscarRestPorNome(nome) {
-    // URL da API (substitua pela URL real da sua API)
+    let divendereço = document.getElementById("endereço");
+    let logoRestaurante = document.getElementById("logoPerfil");
+    let tituloRestaurante = document.getElementById("tituloPerfil");
+  function buscarRestPorId(id) {
+     // URL da API (substitua pela URL real da sua API)
     const enderecoApi = 'categorias.json';
-  
     // Faz a requisição à API
     fetch(enderecoApi)
       .then(response => {
@@ -51,27 +112,61 @@ function buscarRestPorNome(nome) {
         }
         return response.json();
       })
-      .then(data => {
+      .then(data => { 
         // Filtra a pessoa pelo nome
-        resEncontrado = data.restaurantes.find(restaurante => restaurante.nome === nome);
-
-        if (resEncontrado){
-            divRestaurantes.innerHTML = `<p> ${resEncontrado.nome} </p>
-            <p> Telefone:${resEncontrado.telefone} </p>
-            <p> Endereço:${resEncontrado.endereço} </p>
-            `
-        }else{
-            console.log ("Não encontrado");
-        }
-
+        resEncontrado = data.restaurantes.find(restaurante => restaurante.id == id);
         
-         
+        if (resEncontrado){
+          console.log("acerto");
+          logoRestaurante.innerHTML= `
+          <div class="caixaLogo col-12">
+          <img class="logo" src="${resEncontrado.logo}">
+          </div>
           
+          `
+          tituloRestaurante.innerHTML=` <div class="col-12" id="tituloPerfil">
+          <h1>${resEncontrado.nome}</h1>
+      </div>
+      `
+       divendereço.innerHTML=` <p>${resEncontrado.endereço}</p>`
+        }
       })
-      .catch(error => {
-        console.error('Erro:', error);
-      });
+      
+  }
+
+let categoriaBanner = document.getElementById("categoriaId");
+  function buscarCategoPorId(id) {
+     // URL da API (substitua pela URL real da sua API)
+    const enderecoApi = 'categorias.json';
+    // Faz a requisição à API
+    fetch(enderecoApi)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro na requisição');
+        }
+        return response.json();
+      })
+      .then(data => { 
+        // Filtra a pessoa pelo nome
+        categoriaEncontrada = data.restaurantes.find(categoria => categoria.id == id);
+        
+        if (categoriaEncontrada){
+          console.log("acerto");
+          categoriaBanner.innerHTML= `<header class="restaurante" id="categoriaId"
+           style="background-image: url(icones/overlay.png),
+            url(${categoriaEncontrada.banner});">
+            `
+          
+            tituloRestaurante.innerHTML=` <div class="col-12" id="tituloPerfil">
+          <h1>${categoriaEncontrada.nome}</h1>
+      </div>`
+
+        }
+      })
+      
   }
   
+ 
+
   // Chamando a função para buscar a pessoa pelo nome "João da Silva" (substitua pelo nome desejado)
-  buscarRestPorNome('Na Ladeira');
+ 
